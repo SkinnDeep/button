1a44c10cfad73b463133d8d3a719c5f29d2c7f38
KingBib <78509527+KingBib@users.noreply.github.com>
2021-04-14 15:05:49 -0500

    add counter?

--- sounds.js
+++ sounds.js
@@ -8,7 +8,19 @@ for (let i = 0; i < buttons.length; i++) {
     });
 }
 
+//RickRoll Audio
+
 function play() {
     var audio = new Audio('https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Never+Gonna+Give+You+Up-+Original&filename=mz/Mzg1ODMxNTIzMzg1ODM3_JzthsfvUY24.MP3');
     audio.play();
-  }
+}
+
+
+  //Click Counter Function (Not a sound, ik, but still is JS)
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        i++;
+        var cowClick = document.getElementById("counter").innerHTML = "Bib Counter = " + i;
+    })
+};
